2e5f0144335a2e4ae927c5bf257a1f3712d1e749
Raphael Mitsch <r.mitsch@outlook.com>
2023-05-02 20:44:25 +0200

    Merge api.py into prompt.py.

--- spacy_llm/registry/__init__.py
+++ spacy_llm/registry/__init__.py
@@ -1,13 +1,10 @@
 from .util import registry
 from .task import noop_task
-from .api import api_minichain
 from .prompt import minichain_simple_prompt, langchain_simple_prompt
 
 __all__ = [
     # task
     "noop_task",
-    # api
-    "api_minichain",
     # prompt
     "minichain_simple_prompt",
     "langchain_simple_prompt",
