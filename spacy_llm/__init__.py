6f928b92a37d9eff54646e09e71e26362a9eb25b
Kabir Khan <kabir@explosion.ai>
2023-05-08 14:37:58 -0700

    remove isort config for ruff, reorder imports like before

--- spacy_llm/__init__.py
+++ spacy_llm/__init__.py
@@ -1,8 +1,6 @@
-from . import (
-    backends,  # noqa: F401
-    registry,  # noqa: F401
-    tasks,  # noqa: F401
-)
 from .pipeline import llm
+from . import backends  # noqa: F401
+from . import registry  # noqa: F401
+from . import tasks  # noqa: F401
 
 __all__ = ["llm"]
