8bd7e26192c53a30b40cce6a8af06a1c9e9a935a
Raphael Mitsch <r.mitsch@outlook.com>
2023-05-09 23:56:25 +0200

    Add version guards for minichain and langchain in requirements-dev.txt.

--- spacy_llm/compat.py
+++ spacy_llm/compat.py
@@ -7,9 +7,6 @@ else:
     from typing_extensions import Protocol, runtime_checkable, Literal  # noqa: F401
 
 try:
-    # Ensure we import langchain only in the supported Python versions.
-    if sys.version_info[0] == 3 and sys.version_info[1] not in (9, 10, 11):
-        raise ImportError
     import langchain
 
     has_langchain = True
@@ -18,9 +15,6 @@ except (ImportError, AttributeError):
     has_langchain = False
 
 try:
-    # Ensure we import minichain only in the supported Python versions.
-    if sys.version_info[0] == 3 and sys.version_info[1] not in (7, 8, 9):
-        raise ImportError
     import minichain
 
     has_minichain = True
