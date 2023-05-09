446731b380a2bbe1ba0ed9a5e92e7066afa1c9f9
Raphael Mitsch <r.mitsch@outlook.com>
2023-05-09 13:26:24 +0200

    Add simple retry mechanism to REST backend (#33)
    * Add simple retry mechanism.
    
    * Fix retry test.
    
    * Fix retry test.
    
    * Incorporate feedback.
    
    * Fix retry test.
    
    * Fix retry test.
    
    * Fix retry test (for real).
    
    ---------
    
    Co-authored-by: svlandeg <svlandeg@github.com>

--- spacy_llm/tests/backends/test_rest.py
+++ spacy_llm/tests/backends/test_rest.py
@@ -49,3 +49,22 @@ def test_rest_backend_error_handling(strict: bool):
             response["error"]["message"]
             == "The model `x-text-davinci-003` does not exist"
         )
+
+
+def test_retries():
+    """Test retry mechanism."""
+    # Run with 0 tries.
+    nlp = spacy.blank("en")
+    nlp.add_pipe(
+        "llm",
+        config={
+            "task": {"@llm_tasks": "spacy.NoOp.v1"},
+            "backend": {"config": {"model": "text-davinci-003"}, "max_tries": 0},
+        },
+    )
+    with pytest.raises(
+        ConnectionError,
+        match="OpenAI API could not be reached within 30 seconds in 0 attempts. Check your network connection and the "
+        "availability of the OpenAI API.",
+    ):
+        nlp("this is a test")
