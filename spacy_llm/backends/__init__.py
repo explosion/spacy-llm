ee81c7cea86e6e00b313fada2c8d44a4dd525693
Sofie Van Landeghem <svlandeg@users.noreply.github.com>
2023-05-09 11:19:32 +0200

    Dolly/HF backend (#26)
    * add HF backend
    
    * add test from_config
    
    * allow loading not on GPU (but emit warning)
    
    * import accelerate for auto behaviour
    
    * fix dict type
    
    * fix merge leftover
    
    * don't abbreviate HF
    
    * apply reviewer comments
    
    * make backend tests consistent
    
    * raise error if neither cuda or accelerate are installed
    
    * rename to DEFAULT_HF_CONFIG
    
    * install cupy-cuda11x for testing
    
    * skip HF tests if GPU/CUDA not available
    
    * remove torch, transformers and cuda from dev requirements
    
    * split off unrelated changed to separate PR
    
    * make backend specific for Dolly
    
    * rename MODELS and use frozen list
    
    * add comments to 'trust_remote_code'
    
    * underscore private variables

--- spacy_llm/backends/__init__.py
+++ spacy_llm/backends/__init__.py
@@ -1,3 +1,4 @@
+from .dolly import backend_dolly_hf
 from .langchain import backend_langchain, query_langchain
 from .minichain import backend_minichain, query_minichain
 from .rest import backend_rest
@@ -12,4 +13,5 @@ __all__ = [
     "backend_minichain",
     "backend_langchain",
     "backend_rest",
+    "backend_dolly_hf",
 ]
