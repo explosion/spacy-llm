d649ef5c456f0d565e62c4666e80401318b4a5c8
Lj Miranda <ljvmiranda@gmail.com>
2023-05-15 17:45:06 +0800

    Add split_label docstring

--- spacy_llm/util.py
+++ spacy_llm/util.py
@@ -3,6 +3,10 @@ from typing import Iterable, List, Union
 
 def split_labels(labels: Union[str, Iterable[str]]) -> List[str]:
     """Split a comma-separated list of labels.
-    If input is a list already, just strip each entry of the list"""
+    If input is a list already, just strip each entry of the list
+
+    labels (Union[str, Iterable[str]]): comma-separated string or list of labels
+    RETURNS (List[str]): a split and stripped list of labels
+    """
     labels = labels.split(",") if isinstance(labels, str) else labels
     return [label.strip() for label in labels]
