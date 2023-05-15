cb216d784a9e49c4e01acee48efcb27e911612f0
Kabir Khan <kabir@explosion.ai>
2023-05-15 13:28:33 -0700

    fix compat for TypedDict, optional doesn't work until 3.9

--- spacy_llm/compat.py
+++ spacy_llm/compat.py
@@ -2,14 +2,14 @@
 import sys
 
 if sys.version_info[:2] >= (3, 8):  # Python 3.8+
-    from typing import Protocol, runtime_checkable, Literal, TypedDict
+    from typing import Protocol, runtime_checkable, Literal
 else:
-    from typing_extensions import (
-        Protocol,  # noqa: F401
-        runtime_checkable,  # noqa: F401
-        Literal,  # noqa: F401
-        TypedDict,  # noqa: F401
-    )
+    from typing_extensions import Protocol, runtime_checkable, Literal  # noqa: F401
+
+if sys.version_info[:2] >= (3, 9):  # Python 3.8+
+    from typing import TypedDict  # noqa: F401
+else:
+    from typing_extensions import TypedDict  # noqa: F401
 
 try:
     import langchain
