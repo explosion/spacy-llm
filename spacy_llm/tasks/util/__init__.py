1a785b4b003c14e6f8c3198a38bbea8f5e4185a0
Basile Dura <basile@bdura.me>
2023-05-22 16:16:16 +0200

    refactor: rename span_task to span

--- spacy_llm/tasks/util/__init__.py
+++ spacy_llm/tasks/util/__init__.py
@@ -1,5 +1,5 @@
 from .examples import SpanExample
 from .parsing import find_substrings
-from .span_task import SpanTask
+from .span import SpanTask
 
 __all__ = ["SpanExample", "find_substrings", "SpanTask"]
