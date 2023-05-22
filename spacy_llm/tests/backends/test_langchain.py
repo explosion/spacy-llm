73a580f77f37e591698579cedeb6402fc27161bc
Raphael Mitsch <r.mitsch@outlook.com>
2023-05-22 12:16:40 +0200

    Change pytest ignore rule.

--- spacy_llm/tests/backends/test_langchain.py
+++ spacy_llm/tests/backends/test_langchain.py
@@ -1,5 +1,8 @@
+import warnings
+
 import spacy
 import pytest
+from sqlalchemy.exc import MovedIn20Warning
 
 from spacy_llm.compat import has_langchain
 
@@ -16,8 +19,16 @@ PIPE_CFG = {
 
 @pytest.mark.external
 @pytest.mark.skipif(has_langchain is False, reason="LangChain is not installed")
+@pytest.mark.filterwarnings("ignore:^.*pkg_resources.*:DeprecationWarning")
+@pytest.mark.filterwarnings("ignore::sqlalchemy.exc.MovedIn20Warning")
 def test_initialization():
     """Test initialization and simple run"""
     nlp = spacy.blank("en")
     nlp.add_pipe("llm", config=PIPE_CFG)
+
+    warnings.warn("blub pkg_resources", DeprecationWarning)
+    warnings.warn(
+        "The ``declarative_base()`` function is now available as sqlalchemy.orm.declarative_base(). (deprecated since: 2.0)",
+        MovedIn20Warning,
+    )
     nlp("This is a test.")
