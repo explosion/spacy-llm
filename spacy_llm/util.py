23dfc9c5551d8a1d34a6eea93df3209a880e6318
Adriane Boyd <adrianeboyd@gmail.com>
2023-05-24 19:32:07 +0200

    Update util.py

--- spacy_llm/util.py
+++ spacy_llm/util.py
@@ -1,13 +1,13 @@
 from typing import Any, Dict, Union
 from pathlib import Path
-from confection import Config
 
+from confection import Config
 from spacy.util import SimpleFrozenDict, get_sourced_components, load_config
 from spacy.util import load_model_from_config
 
 
 def assemble_from_config(
-    config=Config,
+    config: Config,
 ):
     nlp = load_model_from_config(config, auto_fill=True)
     config = config.interpolate()
@@ -23,10 +23,8 @@ def assemble(
     *,
     overrides: Dict[str, Any] = SimpleFrozenDict(),
 ):
-
     config_path = Path(config_path)
     config = load_config(config_path, overrides=overrides, interpolate=False)
-
     nlp = load_model_from_config(config, auto_fill=True)
     config = config.interpolate()
     sourced = get_sourced_components(config)
