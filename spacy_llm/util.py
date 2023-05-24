f72fedec37594b7a5214aca032c1b419b21b171c
Basile Dura <basile@bdura.me>
2023-05-24 17:21:55 +0200

    feat: assemble accepts a config or a path

--- spacy_llm/util.py
+++ spacy_llm/util.py
@@ -1,17 +1,24 @@
 from typing import Any, Dict, Union
 from pathlib import Path
+from confection import Config
 
 from spacy.util import SimpleFrozenDict, get_sourced_components, load_config
 from spacy.util import load_model_from_config
 
 
 def assemble(
-    config_path: Union[str, Path],
+    config_or_path: Union[str, Path, Config],
     *,
     overrides: Dict[str, Any] = SimpleFrozenDict(),
 ):
-    config_path = Path(config_path)
-    config = load_config(config_path, overrides=overrides, interpolate=False)
+    config: Config
+
+    if isinstance(config_or_path, Config):
+        config = config_or_path
+    else:
+        config_or_path = Path(config_or_path)
+        config = load_config(config_or_path, overrides=overrides, interpolate=False)
+
     nlp = load_model_from_config(config, auto_fill=True)
     config = config.interpolate()
     sourced = get_sourced_components(config)
