e20984bc86439b73a6117b5098d3e9d12a797c11
Basile Dura <bdura@users.noreply.github.com>
2023-05-25 10:33:17 +0200

    feat: REL task (#114)
    * chore: ignore notebooks
    
    * feat: add rel task
    
    * fix: rel template
    
    * docs: add rel example
    
    * feat: clean and simplify rel.py
    
    * docs: mention Doc._.rel in docstrings
    
    * feat: update rel
    
    * docs: update rel example
    
    * docs: document REL task in readme
    
    * Update usage_examples/rel_openai/README.md
    
    Co-authored-by: kadarakos <kadar.akos@gmail.com>
    
    * docs: use en_core_web_md instead of sm
    
    * test: add usage test
    
    * fix: mypy issue
    
    * test: rel configuration
    
    * ci: install en_core_web_md
    
    * fix: test_rel_config assert
    
    * test: rel prediction
    
    * Adjust tests w.r.t. new format.
    
    * ci: install en_core_web_md in external tests
    
    * docs: update description of NER-capable
    
    * fix: check custom rel extension at every call
    
    otherwise multiprocessing won't work
    
    * feat: update rel example to use util.assemble
    
    * chore: move the [initialize] section to the end of the rel example config
    
    * feat: assemble accepts a config or a path
    
    * use assemble in test_rel
    
    * feat: remove useless type hint
    
    * fix: add initialization
    
    * fix: cleaner util
    
    * Update util.py
    
    * Update README.md
    
    Co-authored-by: Adriane Boyd <adrianeboyd@gmail.com>
    
    ---------
    
    Co-authored-by: kadarakos <kadar.akos@gmail.com>
    Co-authored-by: Raphael Mitsch <r.mitsch@outlook.com>
    Co-authored-by: Adriane Boyd <adrianeboyd@gmail.com>

--- spacy_llm/util.py
+++ spacy_llm/util.py
@@ -1,8 +1,21 @@
 from typing import Any, Dict, Union
 from pathlib import Path
 
+from confection import Config
 from spacy.util import SimpleFrozenDict, get_sourced_components, load_config
 from spacy.util import load_model_from_config
+
+
+def assemble_from_config(
+    config: Config,
+):
+    nlp = load_model_from_config(config, auto_fill=True)
+    config = config.interpolate()
+    sourced = get_sourced_components(config)
+    nlp._link_components()
+    with nlp.select_pipes(disable=[*sourced]):
+        nlp.initialize()
+    return nlp
 
 
 def assemble(
