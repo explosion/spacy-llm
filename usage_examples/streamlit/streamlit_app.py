61d936883ceefc0ec192bc869ceecef1729083de
Victoria Slocum <victorialslocum@gmail.com>
2023-05-26 11:27:14 +0200

    apply suggestions from code review (1)

--- usage_examples/streamlit/streamlit_app.py
+++ usage_examples/streamlit/streamlit_app.py
@@ -3,6 +3,57 @@ from spacy_streamlit import visualize_ner, visualize_textcat
 import streamlit as st
 import os
 
+NER_CONFIG = """
+[nlp]
+lang = "en"
+pipeline = ["llm"]
+batch_size = 128
+
+[components]
+
+[components.llm]
+factory = "llm"
+
+[components.llm.backend]
+@llm_backends = "spacy.REST.v1"
+api = "OpenAI"
+config = {"model": "gpt-3.5-turbo", "temperature": 0.3}
+
+[components.llm.task]
+@llm_tasks = "spacy.NER.v1"
+labels = PERSON,ORGANISATION,LOCATION
+examples = null
+
+[components.llm.task.normalizer]
+@misc = "spacy.LowercaseNormalizer.v1"
+"""
+
+TEXTCAT_CONFIG = """
+[nlp]
+lang = "en"
+pipeline = ["llm"]
+batch_size = 128
+
+[components]
+
+[components.llm]
+factory = "llm"
+
+[components.llm.backend]
+@llm_backends = "spacy.REST.v1"
+api = "OpenAI"
+config = {"model": "gpt-3.5-turbo", "temperature": 0.3}
+
+[components.llm.task]
+@llm_tasks = "spacy.TextCat.v1"
+labels = COMPLIMENT,INSULT
+examples = null
+exclusive_classes = true
+
+[components.llm.task.normalizer]
+@misc = "spacy.LowercaseNormalizer.v1"
+"""
+
 DEFAULT_TEXT = "Ernest Hemingway, born in Illinois, is generally considered one of the best authors of his time."
 
 st.title("spacy-llm Streamlit Demo")
@@ -18,10 +69,12 @@ st.markdown(
     """
 )
 
-os.environ["OPENAI_API_KEY"] = st.text_input("Your OpenAI API key", type="password")
+os.environ["OPENAI_API_KEY"] = st.text_input(
+    "Your OpenAI API key", type="password", value=os.environ.get("OPENAI_API_KEY", "")
+)
 text = st.text_area("Text to analyze", DEFAULT_TEXT, height=70)
 
-if os.environ.get("OPENAI_API_KEY"):
+if os.environ["OPENAI_API_KEY"]:
     textcat_config = util.load_config(
         "usage_examples/streamlit/openai_textcat_zeroshot.cfg"
     )
@@ -38,12 +91,13 @@ if os.environ.get("OPENAI_API_KEY"):
     nlp = models[selected_model]
     doc = nlp(text)
     prompt = "\n".join([str(prompt) for prompt in nlp.get_pipe("llm")._template([doc])])
-    st.sidebar.markdown("## OpenAI Prompt:")
-    st.sidebar.text(prompt)
 
     if selected_model == "textcat":
         visualize_textcat(doc)
     if selected_model == "ner":
         visualize_ner(doc)
+
+    st.markdown("### Prompt:")
+    st.text(prompt)
 else:
     st.error("Input your OpenAI API key")
