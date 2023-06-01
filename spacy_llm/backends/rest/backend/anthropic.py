b35aba7174bb073822f27b43cff2982b06d94b7c
Lj Miranda <ljvmiranda@gmail.com>
2023-06-01 20:08:04 +0800

    Implement Anthropic API request signature

--- spacy_llm/backends/rest/backend/anthropic.py
+++ spacy_llm/backends/rest/backend/anthropic.py
@@ -1,8 +1,9 @@
 import os
 from enum import Enum
-from typing import Dict
+from typing import Any, Dict, Iterable, List, Sized
 
 import requests  # type: ignore[import]
+import srsly  # type: ignore[import]
 from requests import HTTPError
 
 from .base import Backend
@@ -10,6 +11,15 @@ from .base import Backend
 
 class Endpoints(str, Enum):
     COMPLETIONS = "https://api.anthropic.com/v1/complete"
+
+
+class Speaker(str, Enum):
+    """Specifies the prompt prefix for engineering Claude-style prompts
+    c.f. https://console.anthropic.com/docs/prompt-design#what-is-a-prompt
+    """
+
+    HUMAN = "\n\nHuman:"
+    ASST = "\n\nAssistant:"
 
 
 class AnthropicBackend(Backend):
@@ -55,11 +65,60 @@ class AnthropicBackend(Backend):
             )
 
         # Set-up headers
-        headers = {
-            "accept": "application/json",
-            "content-type": "application/json",
-            "x-api-key": api_key,
-        }
-
+        headers = {"X-API-Key": api_key}
         assert api_key is not None
         return headers
+
+    def __call__(self, prompts: Iterable[str]) -> Iterable[str]:
+        headers = {
+            **self._credentials,
+            "Content-Type": "application/json",
+        }
+
+        api_responses: List[str] = []
+        prompts = list(prompts)
+        url = self._url if self._url else self.supported_models[self._config["model"]]
+
+        def _request(json_data: Dict[str, Any]) -> Dict[str, Any]:
+            r = self.retry(
+                call_method=requests.post,
+                url=url,
+                headers=headers,
+                json={**json_data, **self._config},
+                timeout=self._max_request_time,
+            )
+
+            try:
+                r.raise_for_status()
+            except HTTPError as ex:
+                res_content = srsly.json_loads(r.content.decode("utf-8"))
+                # Include specific error message in exception.
+                raise ValueError(
+                    f"Request to Anthropic API failed: {res_content.get('error', {})}"
+                ) from ex
+            responses = r.json()
+
+            # c.f. https://console.anthropic.com/docs/api/errors
+            if "error" in responses:
+                if self._strict:
+                    raise ValueError(f"API call failed: {responses}.")
+                else:
+                    assert isinstance(prompts, Sized)
+                    return {"error": [srsly.json_dumps(responses)] * len(prompts)}
+            return responses
+
+        responses = [
+            _request({"prompt": f"{Speaker.HUMAN}{prompt}{Speaker.ASST}"})
+            for prompt in prompts
+        ]
+
+        if "error" in responses:
+            return responses["error"]
+        assert len(responses["choices"]) == len(prompts)
+
+        for response in responses:
+            if "completion" in response:
+                api_responses.append(response["completion"])
+            else:
+                api_responses.append(srsly.json_dumps(response))
+        return api_responses
