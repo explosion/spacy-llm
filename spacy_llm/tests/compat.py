52fbc385f9a034c9e62aa9ae4002858ffcef6f90
Lj Miranda <ljvmiranda@gmail.com>
2023-06-02 11:41:01 +0800

    Add compat for Anthropic API key

--- spacy_llm/tests/compat.py
+++ spacy_llm/tests/compat.py
@@ -4,3 +4,9 @@ if os.getenv("OPENAI_API_KEY") is None:
     has_openai_key = False
 else:
     has_openai_key = True
+
+
+if os.getenv("ANTHROPIC_API_KEY") is None:
+    has_anthropic_key = False
+else:
+    has_anthropic_key = True
