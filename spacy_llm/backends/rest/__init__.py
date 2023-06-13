7f81b94159b399af55989273968d21cbf9189611
Raphael Mitsch <r.mitsch@outlook.com>
2023-06-13 16:29:28 +0200

    Format rest/__init__.py.

--- spacy_llm/backends/rest/__init__.py
+++ spacy_llm/backends/rest/__init__.py
@@ -1,4 +1,12 @@
-from . import anthropic, base, cohere, openai, noop
+from . import anthropic, base, cohere, noop, openai
 from .registry import backend_rest, supported_apis
 
-__all__ = ["anthropic", "base", "cohere", "openai", "noop", "supported_apis", "backend_rest"]
+__all__ = [
+    "anthropic",
+    "base",
+    "cohere",
+    "openai",
+    "noop",
+    "supported_apis",
+    "backend_rest",
+]
