514db77ebe3f1e5f20268b7625bde95c3efa0655
Basile Dura <bdura@users.noreply.github.com>
2023-07-05 13:40:52 +0200

    feat: create examples on init (#163)
    * feat: add NER example gathering from initialization
    
    * feat: add example gathering from init
    
    * feat: add examples from init for REL task
    
    * feat: add examples from init for textcat
    
    * style: consistent naming, updated docstrings
    
    * move example types within files
    
    * rename gather_examples to add_prompt_examples
    
    * black formatting
    
    * rename to prompt_examples to avoid confusion
    
    * refactor initialize loop to avoid having to put all examples in a list
    
    * type fixes
    
    * import fixes
    
    * allow -1 to infer prompt examples from all examples
    
    * black
    
    * fix template rendering
    
    * use set of labels to avoid duplicates internally
    
    * update readme
    
    * sort labels to obtain the same prompts across runs
    
    * fixes
    
    * add functionality to LemmaTask
    
    * fixes
    
    * Update spacy_llm/tasks/lemma.py
    
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>
    
    * Fix test config error.
    
    * Fix test config error.
    
    * Update docstring (to restart tests).
    
    * Update readme.
    
    * Rename infer_prompt_examples to n_prompt_examples.
    
    ---------
    
    Co-authored-by: svlandeg <svlandeg@github.com>
    Co-authored-by: Raphael Mitsch <r.mitsch@outlook.com>
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>

--- spacy_llm/tasks/util/__init__.py
+++ spacy_llm/tasks/util/__init__.py
@@ -1,6 +1,4 @@
-from .examples import SpanExample
 from .parsing import find_substrings
 from .serialization import SerializableTask
-from .span import SpanTask
 
-__all__ = ["SpanExample", "find_substrings", "SpanTask", "SerializableTask"]
+__all__ = ["find_substrings", "SerializableTask"]
