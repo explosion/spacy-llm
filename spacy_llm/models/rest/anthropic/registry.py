40248a1a958e3a2a4071cc521a62d6da3ba48488
Raphael Mitsch <r.mitsch@outlook.com>
2023-07-25 15:50:11 +0200

    Add Claude 2 support (#231)
    * Add Claude 2 support.
    
    * Add Claude-2 support.

--- spacy_llm/models/rest/anthropic/registry.py
+++ spacy_llm/models/rest/anthropic/registry.py
@@ -7,6 +7,40 @@ from ....registry import registry
 from .model import Anthropic, Endpoints
 
 
+@registry.llm_models("spacy.Claude-2.v1")
+def anthropic_claude_2(
+    config: Dict[Any, Any] = SimpleFrozenDict(),
+    name: Literal["claude-2", "claude-2-100k"] = "claude-2",  # noqa: F722
+    strict: bool = Anthropic.DEFAULT_STRICT,
+    max_tries: int = Anthropic.DEFAULT_MAX_TRIES,
+    interval: float = Anthropic.DEFAULT_INTERVAL,
+    max_request_time: float = Anthropic.DEFAULT_MAX_REQUEST_TIME,
+) -> Callable[[Iterable[str]], Iterable[str]]:
+    """Returns Anthropic instance for 'claude-2' model using REST to prompt API.
+    config (Dict[Any, Any]): LLM config arguments passed on to the initialization of the model instance.
+    name (Literal["claude-2", "claude-2-100k"]): Model to use.
+    strict (bool): If True, ValueError is raised if the LLM API returns a malformed response (i. e. any kind of JSON
+        or other response object that does not conform to the expectation of how a well-formed response object from
+        this API should look like). If False, the API error responses are returned by __call__(), but no error will
+        be raised.
+    max_tries (int): Max. number of tries for API request.
+    interval (float): Time interval (in seconds) for API retries in seconds. We implement a base 2 exponential backoff
+        at each retry.
+    max_request_time (float): Max. time (in seconds) to wait for request to terminate before raising an exception.
+    RETURNS (Callable[[Iterable[str]], Iterable[str]]]): Anthropic instance for 'claude-1' model using REST to
+        prompt API.
+    """
+    return Anthropic(
+        name=name,
+        endpoint=Endpoints.COMPLETIONS,
+        config=config,
+        strict=strict,
+        max_tries=max_tries,
+        interval=interval,
+        max_request_time=max_request_time,
+    )
+
+
 @registry.llm_models("spacy.Claude-1.v1")
 def anthropic_claude_1(
     config: Dict[Any, Any] = SimpleFrozenDict(),
