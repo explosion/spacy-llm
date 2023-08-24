69f51113276964a9cc2cf3422ceb8ffd9218b57f
Raphael Mitsch <r.mitsch@outlook.com>
2023-08-24 11:53:16 +0200

    Readd NER Dolly usage example, removed TextCat Dolly one.

--- usage_examples/ner_dolly/run_pipeline.py
+++ usage_examples/ner_dolly/run_pipeline.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 from typing import Optional
 
@@ -13,19 +12,12 @@ Opt = typer.Option
 
 def run_pipeline(
     # fmt: off
-    text: str = Arg("", help="Text to perform text categorization on."),
+    text: str = Arg("", help="Text to perform Named Entity Recognition on."),
     config_path: Path = Arg(..., help="Path to the configuration file to use."),
     examples_path: Optional[Path] = Arg(None, help="Path to the examples file to use (few-shot only)."),
     verbose: bool = Opt(False, "--verbose", "-v", help="Show extra information."),
     # fmt: on
 ):
-    if not os.getenv("OPENAI_API_KEY", None):
-        msg.fail(
-            "OPENAI_API_KEY env variable was not found. "
-            "Set it by running 'export OPENAI_API_KEY=...' and try again.",
-            exits=1,
-        )
-
     msg.text(f"Loading config from {config_path}", show=verbose)
     nlp = assemble(
         config_path,
@@ -36,7 +28,7 @@ def run_pipeline(
     doc = nlp(text)
 
     msg.text(f"Text: {doc.text}")
-    msg.text(f"Categories: {doc.cats}")
+    msg.text(f"Entities: {[(ent.text, ent.label_) for ent in doc.ents]}")
 
 
 if __name__ == "__main__":
