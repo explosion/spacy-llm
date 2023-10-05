b0d3ad2468cf1ac483dbd5754b8459ee6a36ba62
Raphael Mitsch <r.mitsch@outlook.com>
2023-10-05 17:14:49 +0200

    Add langchain Azure OpenAI test (#317)
    * Add langchain Azure OpenAI test.
    
    * Update readme.

--- spacy_llm/tests/models/test_langchain.py
+++ spacy_llm/tests/models/test_langchain.py
@@ -1,7 +1,10 @@
+import os
+
 import pytest
 import spacy
 
 from spacy_llm.compat import has_langchain
+from spacy_llm.tests.compat import has_azure_openai_key
 
 PIPE_CFG = {
     "model": {
@@ -20,4 +23,30 @@ def test_initialization():
     """Test initialization and simple run"""
     nlp = spacy.blank("en")
     nlp.add_pipe("llm", config=PIPE_CFG)
+    nlp("This is a test.")
+
+
+@pytest.mark.external
+@pytest.mark.skipif(has_langchain is False, reason="LangChain is not installed")
+@pytest.mark.skipif(
+    has_azure_openai_key is False, reason="Azure OpenAI key not available"
+)
+def test_initialization_azure_openai():
+    """Test initialization and simple run with Azure OpenAI."""
+    os.environ["OPENAI_API_KEY"] = os.environ["AZURE_OPENAI_KEY"]
+    _pipe_cfg = {
+        "model": {
+            "@llm_models": "langchain.AzureOpenAI.v1",
+            "name": "gpt-35-turbo",
+            "config": {
+                "deployment_name": "gpt-35-turbo",
+                "openai_api_version": "2023-05-15",
+                "openai_api_base": "https://explosion.openai.azure.com/",
+            },
+        },
+        "task": {"@llm_tasks": "spacy.NoOp.v1"},
+    }
+
+    nlp = spacy.blank("en")
+    nlp.add_pipe("llm", config=_pipe_cfg)
     nlp("This is a test.")
