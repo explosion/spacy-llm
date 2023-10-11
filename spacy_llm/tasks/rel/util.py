183421dc463d7f67616e3a11e8d4ba682273ddb9
Raphael Mitsch <r.mitsch@outlook.com>
2023-10-11 10:56:10 +0200

    Drop BaseRELExample.

--- spacy_llm/tasks/rel/util.py
+++ spacy_llm/tasks/rel/util.py
@@ -1,8 +1,4 @@
-import abc
-from typing import Generic, List
-
 from ...compat import BaseModel, validator
-from ...ty import FewshotExample, TaskContraT
 
 
 class RelationItem(BaseModel):
@@ -21,11 +17,3 @@ class EntityItem(BaseModel):
     start_char: int
     end_char: int
     label: str
-
-
-class BaseRELExample(FewshotExample[TaskContraT], abc.ABC, Generic[TaskContraT]):
-    text: str
-    ents: List[EntityItem]
-
-    class Config:
-        arbitrary_types_allowed = True
