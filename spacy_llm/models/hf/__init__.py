e4d7aa508556cf9b30971bfad388cc6c73fcdbd1
Raphael Mitsch <r.mitsch@outlook.com>
2024-02-07 09:52:06 +0100

    Remove Mixtral.

--- spacy_llm/models/hf/__init__.py
+++ spacy_llm/models/hf/__init__.py
@@ -3,7 +3,6 @@ from .dolly import dolly_hf
 from .falcon import falcon_hf
 from .llama2 import llama2_hf
 from .mistral import mistral_hf
-from .mixtral import mixtral_hf
 from .openllama import openllama_hf
 from .phi2 import phi2_hf
 from .stablelm import stablelm_hf
@@ -14,7 +13,6 @@ __all__ = [
     "falcon_hf",
     "llama2_hf",
     "mistral_hf",
-    "mixtral_hf",
     "openllama_hf",
     "phi2_hf",
     "stablelm_hf",
