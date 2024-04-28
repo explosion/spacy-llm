d0043c2cc3336a49b8c36ebcc5a232a6df26dfd6
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-04-28 11:34:47 +0200

    add remaining models

--- spacy_llm/models/rest/ollama/model.py
+++ spacy_llm/models/rest/ollama/model.py
@@ -13,6 +13,7 @@ from ..base import REST
 class Endpoints(str, Enum):
     GENERATE = "http://localhost:11434/api/generate"
     EMBEDDINGS = "http://localhost:11434/api/embeddings"
+    TAGS = "http://localhost:11434/api/tags"
 
 class Ollama(REST):
     @property
@@ -21,8 +22,14 @@ class Ollama(REST):
         return {}
     
     def _verify_auth(self) -> None:
-        # TODO: Verify connectivity to Ollama server
-        pass
+        # Healthcheck: Verify connectivity to Ollama server
+        try:
+            r = requests.get(Endpoints.TAGS.value, timeout=5)
+            r.raise_for_status()
+        except (requests.exceptions.RequestException, HTTPError) as ex:
+            raise ValueError(
+                "Failed to connect to the Ollama server. Please ensure that the server is up and running."
+            ) from ex
 
     def __call__(self, prompts: Iterable[Iterable[str]]) -> Iterable[Iterable[str]]:
         headers = {
@@ -76,5 +83,85 @@ class Ollama(REST):
     @staticmethod
     def _get_context_lengths() -> Dict[str, int]:
         return {
-            "mistral": 4096
+            "llama3": 4096,
+            "phi3": 4096,
+            "wizardlm2": 4096,
+            "mistral": 4096,
+            "gemma": 4096,
+            "mixtral": 47000,
+            "llama2": 4096,
+            "codegemma": 4096,
+            "command-r": 35000,
+            "command-r-plus": 35000,
+            "llava": 4096,
+            "dbrx": 4096,
+            "codellama": 4096,
+            "qwen": 4096,
+            "dolphin-mixtral": 47000,
+            "llama2-uncensored": 4096,
+            "mistral-openorca": 4096,
+            "deepseek-coder": 4096,
+            "phi": 4096,
+            "dolphin-mistral": 47000,
+            "nomic-embed-text": 4096,
+            "nous-hermes2": 4096,
+            "orca-mini": 4096,
+            "llama2-chinese": 4096,
+            "zephyr": 4096,
+            "wizard-vicuna-uncensored": 4096,
+            "openhermes": 4096,
+            "vicuna": 4096,
+            "tinyllama": 4096,
+            "tinydolphin": 4096,
+            "openchat": 4096,
+            "starcoder2": 4096,
+            "wizardcoder": 4096,
+            "stable-code": 4096,
+            "starcoder": 4096,
+            "neural-chat": 4096,
+            "yi": 4096,
+            "phind-codellama": 4096,
+            "starling-lm": 4096,
+            "wizard-math": 4096,
+            "falcon": 4096,
+            "dolphin-phi": 4096,
+            "orca2": 4096,
+            "dolphincoder": 4096,
+            "mxbai-embed-large": 4096,
+            "nous-hermes": 4096,
+            "solar": 4096,
+            "bakllava": 4096,
+            "sqlcoder": 4096,
+            "medllama2": 4096,
+            "nous-hermes2-mixtral": 47000,
+            "wizardlm-uncensored": 4096,
+            "dolphin-llama3": 4096,
+            "codeup": 4096,
+            "stablelm2": 4096,
+            "everythinglm": 16384,
+            "all-minilm": 4096,
+            "samantha-mistral": 4096,
+            "yarn-mistral": 128000,
+            "stable-beluga": 4096,
+            "meditron": 4096,
+            "yarn-llama2": 128000,
+            "deepseek-llm": 4096,
+            "llama-pro": 4096,
+            "magicoder": 4096,
+            "stablelm-zephyr": 4096,
+            "codebooga": 4096,
+            "codeqwen": 4096,
+            "mistrallite": 8192,
+            "wizard-vicuna": 4096,
+            "nexusraven": 4096,
+            "xwinlm": 4096,
+            "goliath": 4096,
+            "open-orca-platypus2": 4096,
+            "wizardlm": 4096,
+            "notux": 4096,
+            "megadolphin": 4096,
+            "duckdb-nsql": 4096,
+            "alfred": 4096,
+            "notus": 4096,
+            "snowflake-arctic-embed": 4096
         }
